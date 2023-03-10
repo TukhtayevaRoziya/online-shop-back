ac304e05a8e06342e81e6f2b5edc39d9e7f3cb8c
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2023-03-10 07:29:10 +0500

    change

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const cors = require("cors"); 
-app.use(cors()); 
+app.use(cors());
 
-// mongoose.set('strictQuery', false);
+mongoose.set('strictQuery', false);
 
 mongoose.connect("mongodb+srv://roziya:onlineShop@cluster0.xkwrwwv.mongodb.net/online-shop", {
   useNewUrlParser: true,
@@ -17,10 +17,10 @@ db.once("open", () => {
   console.log("Connected to Mongodb database...");
 });
 
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json())
 app.use('/', require('./routes/auth'))
 // app.use('/api', require('./routes/home'))
-// app.use("/images", express.static("/"));
+app.use("/images", express.static("images"));
 
 app.listen(3001, ()=>console.log('Server has been started'))
